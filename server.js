4f7c31fabc6893bb84f18c7002803ee354469431
varunprabhakaran22 <50875768+varunprabhakaran22@users.noreply.github.com>
2019-10-23 00:41:39 +0530

    creating the route for sending data to html

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const cors = require("cors");
-const app = express();
 const mongodb = require("mongodb");
+const app = express();
 const port = 3000;
 
 app.use(cors())
+app.listen(port, ()=> {
+    console.log("Port 3000 running on browser...");
+})
 
+app.get("/", (req,res)=>{
+    res.send("Hey Varun");
+})
+// module.exports(app)
