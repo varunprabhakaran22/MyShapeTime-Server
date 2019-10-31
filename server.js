eccd40d01df12f3566896f880760504734a5d646
varunprabhakaran22 <50875768+varunprabhakaran22@users.noreply.github.com>
2019-10-31 10:59:50 +0530

    installing the body parser

--- server.js
+++ server.js
@@ -2,11 +2,14 @@ const express = require("express");
 const cors = require("cors");
 const mongodb = require("mongodb");
 const app = express();
-const port = 3000;
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+// const port = require("dotenv")
 
 app.use(cors())
-app.listen(port, ()=> {
-    console.log("Port 3000 running on browser...");
+app.listen(3000, ()=> {
+    console.log("Your port");
 })
 
 app.get("/", (req,res)=>{
